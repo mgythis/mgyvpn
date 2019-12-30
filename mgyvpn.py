e22b80076edf35fdb6dcf533493aa752e5238574
mgythis <erthis@gmail.com>
2019-12-30 17:16:23 +0100

    Create mgyvpn.py

---
+++ mgyvpn.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.6
+# -*- coding: utf8 -*-
+  """mgyvpn.py
+  Ce script python permet d'automatiser l'installation d'OpenVPN avec chiffrement sous easy-rsa
+  entre un noeud serveur et des noeuds clients
+  Il a été réalisé et testé sous Ubuntu Server LTS 18.04
+  Il fait appel à un ficher de configuration yaml"""
+  
